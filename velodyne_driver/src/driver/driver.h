0f91d443c482211b8547403cf4b086c131f0a320
Bernard Gingold <beniekg@gmail.com>
2018-01-30 14:24:12 +0100

    Add files via upload

---
+++ velodyne_driver/src/driver/driver.h
@@ -0,0 +1,91 @@
+/* -*- mode: C++ -*- */
+/*
+ *  Copyright (C) 2012 Austin Robot Technology, Jack O'Quin
+ * 
+ *  License: Modified BSD Software License Agreement
+ *
+ *  $Id$
+ */
+
+/** \file
+ *
+ *  ROS driver interface for the Velodyne 3D LIDARs
+ */
+
+#ifndef _VELODYNE_DRIVER_H_
+#define _VELODYNE_DRIVER_H_ 1
+
+#include <string>
+#include <ros/ros.h>
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/publisher.h>
+#include <dynamic_reconfigure/server.h>
+
+#include <velodyne_driver/input.h>
+#include <velodyne_driver/VelodyneNodeConfig.h>
+
+namespace velodyne_driver
+{
+  /*namespace {
+    double g_rpm; // global (header file scope)
+    
+    void set_rpm(const double rpm) { // Call this function before executing callback.
+      g_rpm = rpm;
+    }*/
+  }
+
+class VelodyneDriver
+{
+public:
+
+  VelodyneDriver(ros::NodeHandle node,
+                 ros::NodeHandle private_nh
+                 );
+  ~VelodyneDriver() {}
+
+  bool poll(void);
+
+  void set_rpm(const double);
+
+  void do_dynamic_rpm_reconfig(void);
+
+private:
+
+  ///Callback for dynamic reconfigure
+  void callback(velodyne_driver::VelodyneNodeConfig &config,
+              uint32_t level);
+  // Callback for dynamic reconfigure of RPM
+  void callback_update_rpm(velodyne_driver::VelodyneNodeConfig &config,
+              uint32_t level);
+  ///Pointer to dynamic reconfigure service srv_
+  boost::shared_ptr<dynamic_reconfigure::Server<velodyne_driver::
+              VelodyneNodeConfig> > srv_;
+  ////Pointer to dynamic reconfigure service srv2_ used to reconfigure RPM.
+  boost::shared_ptr<dynamic_reconfigure::Server<velodyne_driver::
+               VelodyneNodeConfig> > srv2_;
+
+  // configuration parameters
+  struct
+  {
+    std::string frame_id;            ///< tf frame ID
+    std::string model;               ///< device model name
+    int    npackets;                 ///< number of packets to collect
+    double rpm;                      ///< device rotation rate (RPMs)
+    int cut_angle;                   ///< cutting angle in 1/100°
+    double time_offset;              ///< time in seconds added to each velodyne time stamp
+  } config_;
+
+  boost::shared_ptr<Input> input_;
+  ros::Publisher output_;
+
+  /** diagnostics updater */
+  diagnostic_updater::Updater diagnostics_;
+  double diag_min_freq_;
+  double diag_max_freq_;
+  boost::shared_ptr<diagnostic_updater::TopicDiagnostic> diag_topic_;
+  double rpm_;
+};
+
+} // namespace velodyne_driver
+
+#endif // _VELODYNE_DRIVER_H_
